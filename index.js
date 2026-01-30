cf8c0d67e4e7eb98653115ec503a0a94cb3faa9f
2milliongunguy <2milliongunguy@gmail.com>
2026-01-30 22:11:06 +0000

    genuinely good luck, i am now done wiwith this, maybe

--- index.js
+++ index.js
@@ -1,88 +1,205 @@
-/**
- *
- * @param {string} _p anything you put in here runs
- * enjoy this shit "mini" language
- */
-function _0xabc123(_p) {
-  let _c = _p.split("");
-  let _m = Array(20).fill(0);
-  let _x = 0;
-
-  for (let _i = 0; _i < _c.length; _i++) {
-    let _k = _c[_i];
-
-    switch (_k) {
-      case String.fromCharCode(65):
-        _m[_x]++;
-        break;
-      case String.fromCharCode(66):
-        _x = (_x + 1) % _m.length;
-        break;
-      case String.fromCharCode(67):
-        process["std" + "out"]["wri" + "te"](
-          String["fromChar" + "Code"](_m[_x]),
-        );
-        break;
-      case String.fromCharCode(68):
-        if (_i + 1 < _c.length) {
-          const _t = Buffer.from("41423a42433a43443a4441", "hex")
-            .toString()
-            .split(":")
-            .reduce(
-              (o, p) => (
-                (o[String.fromCharCode(parseInt(p.slice(0, 2), 16))] =
-                  String.fromCharCode(parseInt(p.slice(2, 4), 16))),
-                o
-              ),
-              {},
-            );
-
-          _c[_i + 1] = _t[_c[_i + 1]] || "A";
+const _0x4f2a = (Ι, l, ...ı) => Ι[l](...ı);
+const _0x7b1e = (...І) => І[0x0][І[0x1]];
+const _0x9c3d = (l) => Buffer["from"](l, "hex")["toString"]();
+const _0x2e8f = (O, o) => O[_0x9c3d("636861724174")](o);
+const _0xb4d1 = (...l1) => String[_0x9c3d("66726f6d43686172436f6465")](...l1);
+const _0x6a9c = {
+  [_0xb4d1(0x41)]: (lI, l1, l) => ((lI[l1] = (lI[l1] + (0x1 | 0x0)) | 0x0), {}),
+  [_0xb4d1(0x42)]: (lI, l1, l) => ({
+    l1: (l1 + 0x1) % lI[_0x9c3d("6c656e677468")],
+  }),
+  [_0xb4d1(0x43)]: (lI, l1) => (
+    _0x4f2a(
+      _0x7b1e(
+        _0x7b1e(
+          _0x7b1e(globalThis, _0x9c3d("70726f63657373")),
+          _0x9c3d("7374646f7574"),
+        ),
+        _0x9c3d("7772697465"),
+      ),
+      _0x9c3d("63616c6c"),
+      process.stdout,
+      _0xb4d1(lI[l1]),
+    ),
+    {}
+  ),
+  [_0xb4d1(0x44)]: (lI, l1, l, lll, Ιl) => (
+    (lll[l + (0x1 | 0x0)] =
+      _0x7b1e(
+        (Ιl = Buffer[_0x9c3d("66726f6d")](
+          "41423a42433a43443a4441",
+          _0x9c3d("686578"),
+        )
+          [_0x9c3d("746f537472696e67")]()
+          [_0x9c3d("73706c6974")](_0xb4d1(0x3a))
+          [_0x9c3d("726564756365")](
+            (O, o) => (
+              (O[
+                _0xb4d1(
+                  _0x4f2a(
+                    parseInt,
+                    _0x9c3d("63616c6c"),
+                    parseInt,
+                    _0x4f2a(o, _0x9c3d("736c696365"), 0x0, 0x2),
+                    0x10,
+                  ),
+                )
+              ] = _0xb4d1(
+                _0x4f2a(
+                  parseInt,
+                  _0x9c3d("63616c6c"),
+                  parseInt,
+                  _0x4f2a(o, _0x9c3d("736c696365"), 0x2, 0x4),
+                  0x10,
+                ),
+              )),
+              O
+            ),
+            {},
+          )),
+        lll[l + (0x1 | 0x0)],
+      ) || _0xb4d1(0x41)),
+    {}
+  ),
+  [_0xb4d1(0x21)]: () => (
+    _0x4f2a(
+      _0x7b1e(
+        _0x7b1e(globalThis[_0x9c3d("70726f63657373")], _0x9c3d("7374646f7574")),
+        _0x9c3d("7772697465"),
+      ),
+      _0x9c3d("63616c6c"),
+      process.stdout,
+      _0xb4d1(0xa),
+    ),
+    _0x7b1e(globalThis, _0x9c3d("70726f63657373"))[_0x9c3d("65786974")](0x0),
+    {}
+  ),
+  [_0xb4d1(0x3f)]: () => ({ l1: 0x0 }),
+  [_0xb4d1(0x5a)]: (lI, l1) => {
+    let Il = lI[l1];
+    lI[l1] = 0x0;
+    while ((Il | 0x0) > 0x0)
+      ((l1 = (l1 + 0x1) % lI[_0x9c3d("6c656e677468")]), (Il = Il - 0x1));
+    return { l1 };
+  },
+};
+const _0x1a7f = (O) =>
+  ((Il) =>
+    [...Il][_0x9c3d("726564756365")](
+      (Ιl, l) => (
+        (l = (
+          _0x6a9c[_0x2e8f(Ιl[_0x9c3d("6c6c6c")], Ιl[_0x9c3d("6c")])] ||
+          (() => {
+            const lI =
+              _0x7b1e(
+                [
+                  ..._0x4f2a(
+                    Array,
+                    _0x9c3d("66726f6d"),
+                    Buffer["from"](
+                      Buffer["from"](
+                        "SEVMTE86V09STEQ6UDo0NDpBOlNYWVo=",
+                        _0x9c3d("626173653634"),
+                      )["toString"](),
+                      _0x9c3d("626173653634"),
+                    )
+                      ["toString"]()
+                      [_0x9c3d("73706c6974")](_0xb4d1(0x3a)),
+                  ),
+                ],
+                _0x4f2a(
+                  Math[_0x9c3d("666c6f6f72")],
+                  _0x9c3d("63616c6c"),
+                  Math,
+                  Math[_0x9c3d("72616e646f6d")]() * 0x6,
+                ),
+              ) + "";
+            const l1 = Buffer["from"](
+              [
+                ..._0x4f2a(
+                  Array,
+                  _0x9c3d("66726f6d"),
+                  `${_0x9c3d("4572726f72")}: ${_0x9c3d("556e6b6e6f776e")} ${_0x9c3d("696e737472756374696f6e")} '${_0x2e8f(Ιl[_0x9c3d("6c6c6c")], Ιl[_0x9c3d("6c")])}' ${_0x9c3d("656e636f756e7465726564")} ${_0x9c3d("6174")} ${_0x9c3d("706f736974696f6e")} ${Ιl[_0x9c3d("6c")]}. ${_0x4f2a(lI, _0x9c3d("726570656174"), 0xa)}`[
+                    _0x9c3d("73706c6974")
+                  ](""),
+                )[_0x9c3d("6d6170")]((l) =>
+                  _0xb4d1(
+                    _0x4f2a(l, _0x9c3d("63686172436f64654174"), 0x0) +
+                      0x1 +
+                      0x0,
+                  ),
+                ),
+              ][_0x9c3d("6a6f696e")](""),
+            )
+              ["toString"]()
+              [_0x9c3d("73706c6974")]("")
+              [_0x9c3d("736f7274")](() => Math[_0x9c3d("72616e646f6d")]() - 0.5)
+              [_0x9c3d("6a6f696e")]("");
+            throw new (_0x7b1e(globalThis, _0x9c3d("4572726f72")))(l1);
+          })
+        )(
+          ...[
+            Ιl[_0x9c3d("6c49")],
+            Ιl[_0x9c3d("6c31")],
+            Ιl[_0x9c3d("6c")],
+            Ιl[_0x9c3d("6c6c6c")],
+            0x0,
+          ],
+        )),
+        _0x4f2a(Math, _0x9c3d("72616e646f6d")) <
+        +[..._0x4f2a(Array, "from", { length: 0x4 })]["reduce"](
+          (ll) => ll / (0xa | 0x0),
+          0x1 | 0x0,
+        )
+          ? (() => {
+              let ll = Buffer["from"](
+                "Unexpected error at line: 144 column: 42. ",
+                _0x9c3d("7574662d38"),
+              )["toString"]();
+              for (
+                let IΙ = 0x0;
+                IΙ < (0x1f4 | 0x0);
+                IΙ = (IΙ + (0x1 | 0x0)) | 0x0
+              )
+                ll +=
+                  "" +
+                  _0xb4d1(
+                    (0x41 | 0x0) +
+                      _0x4f2a(
+                        Math[_0x9c3d("666c6f6f72")],
+                        _0x9c3d("63616c6c"),
+                        Math,
+                        Math[_0x9c3d("72616e646f6d")]() * (0x1a | 0x0),
+                      ),
+                  );
+              throw new (_0x7b1e(globalThis, _0x9c3d("4572726f72")))(
+                Buffer[_0x9c3d("66726f6d")](ll)["toString"](
+                  _0x9c3d("626173653634"),
+                ),
+              );
+            })()
+          : 0x0,
+        {
+          lI: Ιl[_0x9c3d("6c49")],
+          l1: l[_0x9c3d("6c31")] ?? Ιl[_0x9c3d("6c31")],
+          l: (Ιl[_0x9c3d("6c")] + (0x1 | 0x0)) | 0x0,
+          lll: Ιl[_0x9c3d("6c6c6c")],
         }
-        break;
-      case String.fromCharCode(33):
-        process.stdout.write("\n");
-        process.exit(0);
-      case String.fromCharCode(63):
-        _x = 0;
-        break;
-      case "Z":
-        let _a = _m[_x];
-        _m[_x] = 0;
-        while (_a > 0) {
-          _x = (_x + 1) % _m.length;
-          _a--;
-        }
-        break;
-      default:
-        "";
-        const _g = ["HELLO", "WORLD", "P", "44", "A", "SXYZ"];
-        const _r = _g[Math.floor(Math.random() * _g.length)];
-        const _em = `Error: Unknown instruction '${_k}' encountered at position ${_i}. ${_r.repeat(
-          10,
-        )}`;
-        const _of = _em
-          .split("")
-          .map((_chr) => String.fromCharCode(_chr.charCodeAt(0) + 1))
-          .join("");
-        const _sf = _of
-          .split("")
-          .sort(() => Math.random() - 0.5)
-          .join("");
-        throw new Error(`${_sf}`);
-    }
-    if (Math.random() < +[...Array(4)].reduce((a) => a / 10, 1)) {
-      let _b = "Unexpected error at line: 144 column: 42. ";
-      for (let _i = 0; _i < 500; _i++) {
-        _b += String.fromCharCode(65 + Math.floor(Math.random() * 26));
-      }
-      const _en = Buffer.from(_b).toString("base64");
-      throw new Error(`${_en}`);
-    }
-  }
-}
+      ),
+      {
+        lI: _0x4f2a(
+          Array,
+          _0x9c3d("66726f6d"),
+          { [_0x9c3d("6c656e677468")]: 0x14 },
+          () => 0x0,
+        ),
+        l1: 0x0,
+        l: 0x0,
+        lll: Il,
+      },
+    ))(O);
 
-_0xabc123(
+_0x1a7f(
   "A".repeat(72) +
     "CB" +
     "A".repeat(69) +
